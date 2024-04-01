832ca492027c8ba157435e18fdf54d42bbf83ce5
iamlydial <lydia.lavecchia@gmail.com>
2024-04-01 20:30:38 +0200

    feat: Implement user schema and model with Mongoose
    - Define interfaces for User and UserDocument to represent user data and Mongoose document.
    - Create a Mongoose schema for the User model, including fields for email, username, and password.
    - Add validation for the email field using the validator package to ensure it's a valid email address.
    - Hash the user's password before saving using bcryptjs and a pre-save hook.
    - Implement a method to validate user passwords against hashed passwords stored in the database.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,32 @@
-console.log('Hello Lydia');
+import express from "express";
+import {createServer} from 'http';
+import { Server } from "socket.io";
+import mongoose, { Mongoose, mongo } from "mongoose";
+
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+//express
+app.get('/', (req, res) => {
+    res.send("API is up");
+})
+
+//socket io 
+io.on('connection', ()=>{
+    console.log("connect");
+})
+
+//mongoose
+mongoose.connect('mongodb://localhost:27017/trello-clone').then(()=>{
+    console.log('connected to mongo db')
+    //http to start server 
+    httpServer.listen(4001, ()=>{
+        console.log('API is listening on port 4001');
+    })
+})
+
+
+
+
