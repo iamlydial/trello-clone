f5104e09508fd99843ec048edab3b3205869fc93
iamlydial <lydia.lavecchia@gmail.com>
2024-04-07 20:30:19 +0200

    Feat: Creating Register Page

--- client/src/auth/auth.module.ts
+++ client/src/auth/auth.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from "@angular/core";
 import { AuthService } from "./services/auth.service";
+import { RegisterComponent } from "./components/register/register.component";
+import { RouterModule, Routes } from "@angular/router";
+
+const routes: Routes = [
+    {
+        path: 'register',
+        component: RegisterComponent
+    }
+]
 
 @NgModule({
-    providers: [AuthService]
+    imports: [RouterModule.forChild(routes)],
+    providers: [AuthService],
+    declarations: [RegisterComponent]
 })
 export class AuthModule{
 
---
+++ client/src/auth/components/register/register.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core'
+
+@Component({
+    selector: 'auth-register',
+    templateUrl: './register.component.html'
+})
+export class RegisterComponent{
+    
+}
