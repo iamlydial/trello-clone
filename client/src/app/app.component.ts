3a3f41a863d452c6a725a6b110ddae8bd21aca41
iamlydial <lydia.lavecchia@gmail.com>
2024-04-07 19:06:24 +0200

    feat: Created authentication module, service, current user interface, and basic functions
    This commit introduces the following changes:
    
    - Implemented an authentication module to manage user authentication.
    - Added a service to handle authentication-related logic.
    - Created an interface for the current user to define its structure.
    - Implemented basic authentication functions within the module.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'eltrello';
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.getCurrentUser().subscribe({
+      next: (res) => {
+        console.log('res',res);
+      },
+      error: (err) => {
+        console.log('err',err);
+        this.authService.setCurrentuser(null);
+      }
+  })
+}
 }
