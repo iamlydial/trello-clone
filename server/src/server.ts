bcc36f33c14726c76516a0e9e1469d37734f0c14
iamlydial <lydia.lavecchia@gmail.com>
2024-04-07 12:47:20 +0200

    feat: Implement user authentication middleware and current user endpoint
    Implemented user authentication middleware to verify JWT tokens and set the authenticated user in the request object. Also added a controller function to retrieve the current authenticated user and return its details.
    
    Changes:
    
    - Added authMiddleware.ts middleware to handle user authentication using JWT tokens.
    - Implemented currentUser controller function in users.ts to retrieve and return details of the current authenticated user.
    - Created ExpressRequestInterface interface to extend the Request interface with a user property to hold the authenticated user.
    
    This commit implements user authentication functionality and exposes an endpoint to retrieve details of the current authenticated user.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,39 +1,36 @@
 import express from "express";
-import {createServer} from 'http';
+import { createServer } from "http";
 import { Server } from "socket.io";
 import mongoose, { Mongoose, mongo } from "mongoose";
-import * as usersControllers from "./controllers/users"
-import bodyParser from 'body-parser';
+import * as usersControllers from "./controllers/users";
+import bodyParser from "body-parser";
+import authMiddleware from "./middlewares/auth";
 
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 //express
-app.get('/', (req, res) => {
-    res.send("API is up");
-})
+app.get("/", (req, res) => {
+  res.send("API is up");
+});
 
-app.post('/api/users', usersControllers.register)
+app.post("/api/users", usersControllers.register);
+app.post("/api/users/login", usersControllers.login);
+app.get("/api/user", authMiddleware, usersControllers.currentUser);
 
-app.post('/api/users/login', usersControllers.login)
-
-//socket io 
-io.on('connection', ()=>{
-    console.log("connect");
-})
+//socket io
+io.on("connection", () => {
+  console.log("connect");
+});
 
 //mongoose
-mongoose.connect('mongodb://localhost:27017/trello-clone').then(()=>{
-    console.log('connected to mongo db')
-    //http to start server 
-    httpServer.listen(4001, ()=>{
-        console.log('API is listening on port 4001');
-    })
-})
-
-
-
-
+mongoose.connect("mongodb://localhost:27017/trello-clone").then(() => {
+  console.log("connected to mongo db");
+  //http to start server
+  httpServer.listen(4001, () => {
+    console.log("API is listening on port 4001");
+  });
+});
