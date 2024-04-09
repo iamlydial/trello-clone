d047cecd494a59e21099f9a6c6eece0b4e8e3a47
iamlydial <lydia.lavecchia@gmail.com>
2024-04-09 21:43:33 +0200

    feat(auth): Implemented AuthService for user authentication
    This commit introduces the following changes:
    
    Created an AuthService to handle user authentication-related functionality.
    Implemented methods to fetch the current user, register new users, set tokens, and update the current user state.
    Utilized Angular's HttpClient to interact with the backend API for user authentication.
    Added type interfaces for the current user and registration requests to ensure type safety.
    Utilized BehaviorSubject to manage the current user's state and provide observability.

--- client/src/auth/services/auth.service.ts
+++ client/src/auth/services/auth.service.ts
@@ -1,22 +1,34 @@
-import { Injectable } from "@angular/core";
-import { BehaviorSubject, Observable } from "rxjs";
-import { CurrentUserInterface } from "../types/currentUser.interface";
-import { HttpClient } from "@angular/common/http";
-import { environment } from "../../environments/environment";
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { CurrentUserInterface } from '../types/currentUser.interface';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { RegisterRequestInterface } from '../types/registerRequest.interface';
 
 @Injectable()
-export class AuthService{
-    currentUser$ = new BehaviorSubject<CurrentUserInterface| null |undefined>(
-        undefined
-    )
-    constructor(private http: HttpClient){}
+export class AuthService {
+  currentUser$ = new BehaviorSubject<CurrentUserInterface | null | undefined>(
+    undefined
+  );
+  constructor(private http: HttpClient) {}
 
-    getCurrentUser(): Observable<CurrentUserInterface>{
-        const url = environment.apiUrl+'/user';
-        return this.http.get<CurrentUserInterface>(url);
-    }
+  getCurrentUser(): Observable<CurrentUserInterface> {
+    const url = environment.apiUrl + '/user';
+    return this.http.get<CurrentUserInterface>(url);
+  }
 
-    setCurrentuser(currentUser: CurrentUserInterface|null): void{
-        this.currentUser$.next(currentUser)
-    }
+  register(
+    registerRequest: RegisterRequestInterface
+  ): Observable<CurrentUserInterface> {
+    const url = (environment.apiUrl = '/users');
+    return this.http.post<CurrentUserInterface>(url, registerRequest);
+  }
+
+  setToken(currentUser: CurrentUserInterface): void {
+    localStorage.setItem('token', currentUser.token);
+  }
+
+  setCurrentuser(currentUser: CurrentUserInterface | null): void {
+    this.currentUser$.next(currentUser);
+  }
 }
