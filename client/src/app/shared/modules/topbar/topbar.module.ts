27798c2a78e7a252683da1b50018620685bea600
iamlydial <lydia.lavecchia@gmail.com>
2024-06-20 19:45:58 +0200

    implement markup for the topbar

--- client/src/app/shared/modules/topbar/topbar.module.ts
+++ client/src/app/shared/modules/topbar/topbar.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { TopbarComponent } from './component/topbar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   declarations: [TopbarComponent],
   exports: [TopbarComponent],
 })
