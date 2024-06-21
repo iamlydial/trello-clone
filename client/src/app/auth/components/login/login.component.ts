bd1ee777403a307565282b37d5dd35431b15c81a
iamlydial <lydia.lavecchia@gmail.com>
2024-06-21 11:47:14 +0200

    setup of socket connection after login / register

--- client/src/app/auth/components/login/login.component.ts
+++ client/src/app/auth/components/login/login.component.ts
@@ -5,37 +5,42 @@ import { RegisterRequestInterface } from '../../types/registerRequest.interface'
 import { HttpErrorResponse } from '@angular/common/http';
 import { LoginRequestInterface } from '../../types/loginRequest.interface';
 import { Router } from '@angular/router';
+import { SocketService } from '../../../shared/services/socket.service';
 
 @Component({
   selector: 'auth-login',
   templateUrl: './login.component.html',
 })
 export class LoginComponent {
-    errorMessage: string|null = null;
+  errorMessage: string | null = null;
 
-    form = this.fb.group({
-        email: ['', Validators.required],
-        password: ['', Validators.required],
+  form = this.fb.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private socketService: SocketService
+  ) {}
+
+  onSubmit(): void {
+    const formValue = this.form.value as LoginRequestInterface; // Cast form value to RegisterRequestInterface
+    this.authService.login(formValue).subscribe({
+      next: (currentUser) => {
+        console.log('currentUser', currentUser);
+        this.authService.setToken(currentUser);
+        this.socketService.setupSocketConnection(currentUser);
+        this.authService.setCurrentuser(currentUser);
+        this.errorMessage = null;
+        this.router.navigateByUrl('/');
+      },
+      error: (err: HttpErrorResponse) => {
+        console.log('err', err.error);
+        this.errorMessage = err.error.emailOrPassword;
+      },
     });
-
-    constructor(private fb: FormBuilder, 
-        private authService: AuthService,
-        private router: Router) {}
-
-    onSubmit(): void {
-        const formValue = this.form.value as LoginRequestInterface; // Cast form value to RegisterRequestInterface
-        this.authService.login(formValue).subscribe({
-            next:(currentUser)=>{
-                console.log('currentUser', currentUser);
-                this.authService.setToken(currentUser);
-                this.authService.setCurrentuser(currentUser);
-                this.errorMessage = null;
-                this.router.navigateByUrl('/');
-            },
-            error: (err: HttpErrorResponse) => {
-                console.log('err', err.error);
-                this.errorMessage = err.error.emailOrPassword;
-            }
-        })
-    }
+  }
 }
--- client/src/app/auth/components/register/register.component.ts
+++ client/src/app/auth/components/register/register.component.ts
@@ -4,39 +4,42 @@ import { AuthService } from '../../services/auth.service';
 import { RegisterRequestInterface } from '../../types/registerRequest.interface'; // Import the interface
 import { HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { SocketService } from '../../../shared/services/socket.service';
 
 @Component({
   selector: 'auth-register',
   templateUrl: './register.component.html',
 })
 export class RegisterComponent {
-    errorMessage: string|null = null;
+  errorMessage: string | null = null;
 
-    form = this.fb.group({
-        email: ['', Validators.required],
-        username: ['', Validators.required],
-        password: ['', Validators.required],
+  form = this.fb.group({
+    email: ['', Validators.required],
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private socketService: SocketService
+  ) {}
+
+  onSubmit(): void {
+    const formValue = this.form.value as RegisterRequestInterface; // Cast form value to RegisterRequestInterface
+    this.authService.register(formValue).subscribe({
+      next: (currentUser) => {
+        console.log('currentUser', currentUser);
+        this.authService.setToken(currentUser);
+        this.socketService.setupSocketConnection(currentUser);
+        this.authService.setCurrentuser(currentUser);
+        this.router.navigateByUrl('/');
+      },
+      error: (err: HttpErrorResponse) => {
+        console.log('err', err.error);
+        this.errorMessage = err.error.join(', ');
+      },
     });
-
-    constructor(
-        private fb: FormBuilder, 
-        private authService: AuthService,
-        private router : Router
-        ) {}
-
-    onSubmit(): void {
-        const formValue = this.form.value as RegisterRequestInterface; // Cast form value to RegisterRequestInterface
-        this.authService.register(formValue).subscribe({
-            next:(currentUser)=>{
-                console.log('currentUser', currentUser);
-                this.authService.setToken(currentUser);
-                this.authService.setCurrentuser(currentUser);
-                this.router.navigateByUrl('/');
-            },
-            error: (err: HttpErrorResponse) => {
-                console.log('err', err.error);
-                this.errorMessage = err.error.join(', ');
-            }
-        })
-    }
+  }
 }
